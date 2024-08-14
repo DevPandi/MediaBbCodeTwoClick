fb819dbba74de0afc4a1e120fac9c83163d287d5
DevPandi <pandi@posteo.de>
2024-08-14 20:53:25 +0200

    Prepare two click variant.

---
+++ _output/extension_hint.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @noinspection PhpIllegalPsrClassPathInspection */
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace DevPandi\MediaBbCodeTwoClick\BbCode
+{
+	class XFCP_MediaBbCode extends \XF\BbCode\Renderer\Html {}
+}
