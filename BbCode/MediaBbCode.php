a3bc08f499301a77de8902d0b451ed46ec79d67a
DevPandi <pandi@posteo.de>
2024-08-15 12:12:18 +0200

    Add build.json

--- BbCode/MediaBbCode.php
+++ BbCode/MediaBbCode.php
@@ -2,10 +2,16 @@
 
 namespace DevPandi\MediaBbCodeTwoClick\BbCode;
 
+use XF;
+
 class MediaBbCode extends XFCP_MediaBbCode
 {
     public function renderTagMedia(array $children, $option, array $tag, array $options)
     {
+        if (XF::visitor()->getValue('load_embedded')) {
+            return parent::renderTagMedia($children, $option, $tag, $options);
+        }
+
         $content = parent::renderTagMedia($children, $option, $tag, $options);
 
         if ($content === '') {
